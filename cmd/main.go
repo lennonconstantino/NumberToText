9e07b5b6991376ae6c2688b01a3425299ab46a2b
lennonconstantino <lennonconstantino@gmail.com>
2025-05-15 21:19:02 -0300

    doing more tests

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println(parseNumberToTest.NumberToText(1))
 	fmt.Println(parseNumberToTest.NumberToText(12))
 	fmt.Println(parseNumberToTest.NumberToText(123))
-	// fmt.Println(parseNumberToTest.NumberToText(123123))
+	fmt.Println(parseNumberToTest.NumberToText(123123))
 	// fmt.Println(parseNumberToTest.NumberToText(123123123))
 	// fmt.Println(parseNumberToTest.NumberToText(123123123123))
 	// fmt.Println(parseNumberToTest.NumberToText(123123123123123))
